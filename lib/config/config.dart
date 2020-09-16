0f1dfb4a53ae10b90b62d888f0bd9d2a53ca492c
yanmyo45 <yanmyokyaw.kantech@gmail.com>
2020-09-16 14:10:12 +0630

    commit for brand new ui changes...

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,1 +1,31 @@
+import 'dart:io';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:flutter/material.dart';
+
 const FILE_PATH = 'http://128.199.167.17:8989/content';
+
+Future<File> pickAndCropImg() async {
+  final picker = ImagePicker();
+  final pickFile = await picker.getImage(source: ImageSource.gallery);
+  File croppedFile = await ImageCropper.cropImage(
+      sourcePath: pickFile.path,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+        CropAspectRatioPreset.ratio3x2,
+        CropAspectRatioPreset.original,
+        CropAspectRatioPreset.ratio4x3,
+        CropAspectRatioPreset.ratio16x9
+      ],
+      androidUiSettings: AndroidUiSettings(
+          toolbarTitle: 'YMK Pos',
+          toolbarColor: Colors.blue,
+          toolbarWidgetColor: Colors.white,
+          initAspectRatio: CropAspectRatioPreset.square,
+          lockAspectRatio: false),
+      iosUiSettings: IOSUiSettings(
+        title: 'YMK Pos',
+        minimumAspectRatio: 1.0,
+      ));
+  return croppedFile;
+}
--- lib/mobile/views/brand/brandnew.dart
+++ lib/mobile/views/brand/brandnew.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:ymk_pos/components/formitems/formitems.dart';
 import 'package:ymk_pos/components/button/mainbutton.dart';
+import 'package:ymk_pos/config/config.dart';
+import 'dart:io';
 
-class BrandNew extends StatelessWidget {
+class BrandNew extends StatefulWidget {
   static String tag = 'brand-new';
+  @override
+  _BrandNewState createState() => _BrandNewState();
+}
+
+class _BrandNewState extends State<BrandNew> {
   final _formKey = GlobalKey<FormState>();
   final TextEditingController _name = TextEditingController();
   final TextEditingController _description = TextEditingController();
+  File coverPhoto;
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Create Brand"),
@@ -23,6 +32,47 @@ class BrandNew extends StatelessWidget {
                   key: _formKey,
                   child: Column(
                     children: <Widget>[
+                      InkWell(
+                        child: Container(
+                          width: width,
+                          height: 200,
+                          margin: EdgeInsets.only(bottom: 15),
+                          decoration: BoxDecoration(color: Colors.black26),
+                          child: coverPhoto == null
+                              ? Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Icon(
+                                      Icons.add_circle_outline,
+                                      color: Colors.white,
+                                    ),
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Text(
+                                      "ADD BRAND PHOTO",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              :
+                              Container(
+                                padding: EdgeInsets.all(5),
+                                child: Image(
+                                  image: FileImage(coverPhoto),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                        ),
+                        onTap: () async {
+                          final croppedFile = await pickAndCropImg();
+                          setState(() {
+                            coverPhoto = croppedFile;
+                          });
+                        },
+                      ),
                       InputField(
                         title: 'Name',
                         controller: _name,
@@ -41,7 +91,7 @@ class BrandNew extends StatelessWidget {
                 ),
               ),
               MainButton(
-                width: MediaQuery.of(context).size.width,
+                width: width,
                 title: "Submit",
                 onPress: () {
                   _formKey.currentState.validate();
